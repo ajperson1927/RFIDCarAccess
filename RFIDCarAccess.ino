49d545850045487255c8806dfcf234d4880330f8
pontiaj2066 <pontiaj2066@gmail.com>
2021-12-24 15:38:26 -0700

    Added sleep mode to test power draw. 40ma to 20ma

--- RFIDCarAccess.ino
+++ RFIDCarAccess.ino
@@ -1,9 +1,23 @@
+#include "avr/interrupt"
+#include "avr/sleep.h"
 void setup() {
   // put your setup code here, to run once:
-
+ Serial.begin(9600);
+ pinMode(LED_BUILTIN, OUTPUT);
+ digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  if (millis() > 5000) {
+    //Serial.print("time\n");
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    sleep_cpu();
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
