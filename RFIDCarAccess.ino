70ee946e4da4c0860c5c7aeb65a363aec6e6421b
pontiaj2066 <pontiaj2066@gmail.com>
2022-02-04 17:40:46 -0700

    Updated pin definitions for everything

--- RFIDCarAccess.ino
+++ RFIDCarAccess.ino
@@ -1,10 +1,19 @@
 #include <EnergySaving.h>
+#include <Adafruit_PN532.h>
+#include <SPI.h>
+#include <Wire.h> 
 
-#define OUTPUT_LED 9
-#define INTERRUPT_PIN 8
+#define FOB_UNLOCK_PIN 0
+#define FOB_LOCK_PIN 1
+#define OUTPUT_LED 6
 #define FOB_POWER_PIN 7
-#define FOB_UNLOCK_PIN 6
-#define FOB_LOCK_PIN 5
+#define INTERRUPT_PIN 8
+
+#define IRQ_PIN 2
+#define RESET_PIN 3
+//#define SDA_PIN 4     These are hardcoded pins. Included
+//#define SCL_PIN 5     for clarity only. Can't be set or changed
+
 
 int fobOnPeriod = 10000;
 unsigned long fobOnTime = 0;
@@ -13,30 +22,33 @@ EnergySaving energySaving;
 
 void setup() {
  
- pinMode(LED_BUILTIN, OUTPUT);
- pinMode(OUTPUT_LED, OUTPUT);
- pinMode(INTERRUPT_PIN, INPUT);
- pinMode(FOB_POWER_PIN, OUTPUT);
- pinMode(FOB_UNLOCK_PIN, OUTPUT);
- pinMode(FOB_LOCK_PIN, OUTPUT);
-
- pinMode(0, OUTPUT);      //temporary 3.3v power until 
- digitalWrite(0, HIGH);   //hooked to external supply
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(OUTPUT_LED, OUTPUT);
+  pinMode(INTERRUPT_PIN, INPUT);
+  pinMode(FOB_POWER_PIN, OUTPUT);
+  pinMode(FOB_UNLOCK_PIN, OUTPUT);
+  pinMode(FOB_LOCK_PIN, OUTPUT);
  
- digitalWrite(FOB_POWER_PIN, LOW);
- digitalWrite(OUTPUT_LED, HIGH);
+  //digitalWrite(LED_BUILTIN, HIGH);
+  //digitalWrite(OUTPUT_LED, HIGH);
 
- energySaving.begin(WAKE_EXT_INTERRUPT, INTERRUPT_PIN, interruptRoutine);
+  Serial.begin(9600);
+  while (!Serial);
+
+  energySaving.begin(WAKE_EXT_INTERRUPT, INTERRUPT_PIN, interruptRoutine);
+  
 }
 
 void loop() {
+  
   if (millis() - fobOnTime > fobOnPeriod) {
-    digitalWrite(FOB_POWER_PIN, LOW);
-    energySaving.standby();
+    //digitalWrite(LED_BUILTIN, LOW);
+    //energySaving.standby();
   }
 }
 
 void interruptRoutine() {
-  digitalWrite(FOB_POWER_PIN, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("INTERRUPTED");
   fobOnTime = millis();
 }
