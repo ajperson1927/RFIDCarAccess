2e03afb760a2be4c87a3ac4427a40f0c1e6a2000
pontiaj2066 <pontiaj2066@gmail.com>
2022-02-08 22:27:57 -0700

    Finished EEPROM functions and initialization

--- RFIDCarAccess.ino
+++ RFIDCarAccess.ino
@@ -18,9 +18,13 @@
 int fobOnPeriod = 10000;
 unsigned long fobOnTime = 0;
 
+//variables for EEEPROM and UID handling
+const int maxUIDLength = 7; //The max length a UID can be. Most tags are 4 or 7 long
+const int maxUIDCount = 10; //The max amount of UIDs can be stored in the EEPROM. Not including the master tag
+const int firstUIDPosition = 1; //first UID after the master tag
+
 EnergySaving energySaving;
 Adafruit_PN532 pn532(IRQ_PIN, RESET_PIN);
-
 
 void setup() {
  
@@ -37,12 +41,29 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  energySaving.begin(WAKE_EXT_INTERRUPT, INTERRUPT_PIN, interruptRoutine);
+  //energySaving.begin(WAKE_EXT_INTERRUPT, INTERRUPT_PIN, interruptRoutine);
   pn532.begin();
   
+  //If EEPROM doesn't exist yet, create and fill it with zeroes
+  if (!EEPROM.isValid()) { 
+    for (int i = 0; i <= (maxUIDCount + 2) * 7; i++) {
+      EEPROM.write(i, 0);
+    }
+    EEPROM.commit();
+  }
+}
+  /*
+  uint8_t uid[maxUIDLength];
+  Serial.println(readUIDFromEEPROM(uid, 1));
+  for (int i = 0; i < maxUIDLength; i++) {
+    Serial.print(uid[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  */
 }
 
-void loop() {
+void loop() { /*
   uint32_t versiondata = pn532.getFirmwareVersion();
   if (!versiondata) {
     Serial.println("PN532 not detected");
@@ -63,7 +84,15 @@ void loop() {
   if (millis() - fobOnTime > fobOnPeriod) {
     //digitalWrite(LED_BUILTIN, LOW);
     //energySaving.standby();
-  }
+  }*/
+
+    //test code. halts loop until something is entered in serial monitor
+    Serial.flush();
+    while (!Serial.available());
+    while (Serial.available()) {
+    Serial.read();
+    }
+    Serial.flush();
 }
 
 void interruptRoutine() {
@@ -72,14 +101,81 @@ void interruptRoutine() {
   fobOnTime = millis();
 }
 
-bool writeUIDToEEPROM(uint8_t uid[], uint8_t uidPosition) {
+//This takes in a tag UID, writes it to the proper indexes
+//in the EEPROM derived from uidIndex, and commits the data.
+//Returns success status
+bool writeUIDIntoEEPROM(uint8_t uid[], uint8_t uidIndex) {
+  int index = (uidIndex) * maxUIDLength + 1;//calculate starting index
   
+  for (int i = 0; i < maxUIDLength; i++) {//Write uid[] into EEPROM
+    EEPROM.write(index + i, uid[i]);
+  }
+  EEPROM.commit();
+  return true;
 }
 
-bool readUIDFromEEPROM(uint8_t uid[], uint8_t uidPosition) {
+//This reads the tag UID from the proper indexes derived from
+//uidIndex, and writes it to the given uid[].
+//Returns success status
+bool readUIDFromEEPROM(uint8_t uid[], uint8_t uidIndex) {
   
+  //If EEPROM doesn't exist yet, do nothing and return false
+  if (!EEPROM.isValid()) return false; 
+  
+  int index = uidIndex * maxUIDLength + 1; //calculate starting index
+  
+  for (int i = 0; i < maxUIDLength; i++) { //Copy EEPROM UID into uid[]
+    uid[i] = EEPROM.read(index + i);
+  }
+  return true;
 }
 
-bool searchUIDInEEPROM(uint8_t uid[], uint8_t uidCount) {
+bool addUIDIntoEEPROMList(uint8_t uid[]) {
+  int currentUIDCount = EEPROM.read(0);
+  if (currentUIDCount >= maxUIDCount + 1) return false;
+  
+  EEPROM.write(0, currentUIDCount + 1);
+  return writeUIDIntoEEPROM(uid, currentUIDCount);
+}
 
+//Removes given UID from the EEPROM, and shifts the UIDs after
+//1 to the left 
+bool removeUIDFromEEPROMList(uint8_t uid[]) {
+  
+  //Finds index of UID we're trying to remove. If it doesn't exist,
+  //or is the master tag, do nothing and return false
+  int uidIndex = searchUIDInEEPROM(uid);
+  if (uidIndex <= 0) return false;
+
+  //Shifts every UID left, starting with the one after target UID
+  for (int i = uidIndex; i < maxUIDCount; i++) {
+     uint8_t newUID[maxUIDLength];
+     readUIDFromEEPROM(newUID, i + 1);
+     writeUIDIntoEEPROM(newUID, i);
+  }
+  EEPROM.write(0, uidIndex);
+  EEPROM.commit();
+  return true;
+}
+
+//Searches the EEPROM for given UID. If a match is found, 
+//it's index is returned. Otherwise a -1 is returned
+int searchUIDInEEPROM(uint8_t uid[]) {
+
+  //iterate through all uids in list
+  for (int i = 0; i < maxUIDCount; i++) {
+    uint8_t currentUID[maxUIDLength];
+    readUIDFromEEPROM(currentUID, i);
+
+    //compare current uid with given uid. If they match, return current index
+    bool arraysMatch = true;
+    for (int j = 0; j < maxUIDLength; j++) {
+      if (currentUID[j] != uid[j]) {
+        arraysMatch = false;
+        break;
+      }
+    }
+    if (arraysMatch) return i;
+  }
+  return -1;
 }
