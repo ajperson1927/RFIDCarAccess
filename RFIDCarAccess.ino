d0c4ddeb5b9fc51f97595899e7b8f8e1b0b00568
pontiaj2066 <pontiaj2066@gmail.com>
2022-02-12 00:28:34 -0700

    Added new comments and cleaned up old ones

--- RFIDCarAccess.ino
+++ RFIDCarAccess.ino
@@ -1,3 +1,11 @@
+/*
+      This is a program that allows one to replace a key fob on a push button car with an RFID access control system.
+      It utilizes a Seeeduino XIAO as the main board, with a PN532 as the RFID antenna. Tag storing is handled by
+      using a simulated EEPROM in fllash storage. Due to it being flash storage, it resets every time the program is
+      uploaded. 
+      The EEPROM contains a simulated array to contain the tag UIDs. The first byte indicates the size of the array.
+      The rest is divided into maxUIDLength byte long UIDs. First one will always be the master tag
+*/
 #include <EnergySaving.h>
 #include <Adafruit_PN532.h>
 #include <SPI.h>
@@ -16,13 +24,14 @@
 //#define SDA_PIN 4     These are hardcoded pins. Included
 //#define SCL_PIN 5     for clarity only. Can't be set or changed
 
-int fobOnPeriod = 30000;
+//These are for tracking different timers the system uses
+int fobOnPeriod = 30000; //How long the fob stays on without input before turning off
 unsigned long fobOnTime;
 
-int tagScanWaitPeriod = 2000;
+int tagScanWaitPeriod = 2000; //How long to wait after scanning a tag before another can be scanned
 unsigned long tagScanTime;
 
-int changeTagTimeout = 5000;
+int changeTagTimeout = 5000; //How long must pass when adding/removing a tag before the action is auto cancelled
 unsigned long tagTimeoutTime;
 
 //variables for EEEPROM and UID handling
@@ -30,9 +39,12 @@ const int maxUIDLength = 7; //The max length a UID can be. Most tags are 4 or 7 
 const int maxUIDCount = 10; //The max amount of UIDs can be stored in the EEPROM. Not including the master tag
 const int firstUIDPosition = 1; //first UID after the master tag
 
+//States for when tags are being scanned
 const int TAGWAITING =      0;
 const int TAGIDLE =         1;
+int tagState = TAGIDLE; //Starting state
 
+//Stated for the main operation of the system
 const int IDLESTATE =       0;
 const int CARONSTATE =      1;
 const int COUNTDOWNSTATE =  2;
@@ -40,11 +52,9 @@ const int ADDMASTERTAG =    3;
 const int ADDNEWTAG =       4;
 const int REMOVETAG =       5;
 const int CLEARALLTAGS =    6;
+int systemState = IDLESTATE; //Starting state
 
-int tagState = TAGIDLE;
-int systemState = IDLESTATE;
-bool newTag = false;
-uint8_t clearUid[maxUIDLength];
+uint8_t clearUid[maxUIDLength]; //UID array full of zeroes
 
 EnergySaving energySaving;
 Adafruit_PN532 pn532(IRQ_PIN, RESET_PIN);
@@ -58,17 +68,14 @@ void setup()
   pinMode(FOB_POWER_PIN, OUTPUT);
   pinMode(FOB_UNLOCK_PIN, OUTPUT);
   pinMode(FOB_LOCK_PIN, OUTPUT);
- 
-  //digitalWrite(LED_BUILTIN, HIGH);
-  //digitalWrite(OUTPUT_LED, HIGH);
+  pinMode(CAR_POWER_PIN, INPUT);
 
-  Serial.begin(9600);
+  Serial.begin(9600); //Debug purposes only. Won't be needed when finished
   while (!Serial);
 
   //energySaving.begin(WAKE_EXT_INTERRUPT, INTERRUPT_PIN, interruptRoutine);
   
-  //If EEPROM doesn't exist yet, create and fill it with zeroes.
-  //Set state to adding master tag
+  //If EEPROM doesn't exist yet, initialize it
   if (!EEPROM.isValid()) { 
     clearEEPROM();
   }
@@ -150,6 +157,7 @@ void loop()
     }
     case ADDNEWTAG:
     {
+      //If timed out, go back to idle
       if (millis() - tagTimeoutTime > changeTagTimeout)
       {
         systemState = IDLESTATE;
@@ -180,6 +188,7 @@ void loop()
     }
     case REMOVETAG:
     {
+      //If timed out, go back to idle
       if (millis() - tagTimeoutTime > changeTagTimeout)
       {
         systemState = IDLESTATE;
@@ -187,14 +196,17 @@ void loop()
       if (validScan)
       {
         int uidIndex = searchUID(uid);
+        //If master tag scanned, start reset proccess
         if (uidIndex == 0)
         {
           systemState = CLEARALLTAGS;
         }
+        //If tag doesn't exist, return to idle state
         else if (uidIndex == -1)
         {
           systemState = IDLESTATE;
         }
+        //If tag exists, remove it and return to idle state
         else
         {
           removeUID(uid);
@@ -225,12 +237,14 @@ void loop()
     }
     case CLEARALLTAGS:
     {
+      //If timed out, go back to idle
       if (millis() - tagTimeoutTime > changeTagTimeout)
       {
         systemState = IDLESTATE;
       }
       if (validScan)
       {
+        //If master tag is scanned, reset system. Otherwise wait for timeout
         int uidIndex = searchUID(uid);
         if (uidIndex == 0)
         {
@@ -241,44 +255,25 @@ void loop()
     }
     case CARONSTATE:
     {
+      //If car turns off, go to countdown state
       if (!digitalRead(CAR_POWER_PIN))
       {
         fobOnTime = millis();
         systemState = COUNTDOWNSTATE;
       }
-      
       break;
     }
   }
-
-  if (millis() - fobOnTime > fobOnPeriod) 
-  {
-    //digitalWrite(LED_BUILTIN, LOW);
-    //energySaving.standby();
-  }
-
-    //test code. halts loop until something is entered in serial monitor
-    /*
-    Serial.flush();
-    while (!Serial.available());
-    while (Serial.available()) 
-    {
-      Serial.read();
-    }
-    Serial.flush();
-    */
 }
 
 void interruptRoutine() 
 {
   //digitalWrite(LED_BUILTIN, HIGH);
   //Serial.println("INTERRUPTED");
-  fobOnTime = millis();
+  //fobOnTime = millis();
 }
 
-//This takes in a tag UID, writes it to the proper indexes
-//in the EEPROM derived from uidIndex, and commits the data.
-//Returns success status
+//This takes in a tag UID, writes it to the proper indexes in the EEPROM derived from uidIndex, and commits the data. Returns success status
 bool changeUID(uint8_t uid[], uint8_t uidIndex) 
 {
   int index = (uidIndex) * maxUIDLength + 1;//calculate starting index
@@ -292,9 +287,7 @@ bool changeUID(uint8_t uid[], uint8_t uidIndex)
   return true;
 }
 
-//This reads the tag UID from the proper indexes derived from
-//uidIndex, and writes it to the given uid[].
-//Returns success status
+//This reads the tag UID from the proper indexes derived from uidIndex, and writes it to the given uid[]. Returns success status
 bool readUID(uint8_t uid[], uint8_t uidIndex) 
 {
   
@@ -319,8 +312,7 @@ bool addUID(uint8_t uid[])
   return changeUID(uid, latestUIDIndex + 1);
 }
 
-//Removes given UID from the EEPROM, and shifts the UIDs after
-//1 to the left 
+//Removes given UID from the EEPROM, and shifts the UIDs after 1 to the left 
 bool removeUID(uint8_t uid[]) 
 {
   
@@ -336,14 +328,13 @@ bool removeUID(uint8_t uid[])
      readUID(newUID, i + 1);
      changeUID(newUID, i);
   }
-  EEPROM.write(0, uidIndex);
+  EEPROM.write(0, EEPROM.read(0) - 1);
   Serial.println("WRITING TO EEPROM");
   EEPROM.commit();
   return true;
 }
 
-//Searches the EEPROM for given UID. If a match is found, 
-//it's index is returned. Otherwise a -1 is returned
+//Searches the EEPROM for given UID. If a match is found, its index is returned. Otherwise a -1 is returned
 int searchUID(uint8_t uid[]) 
 {
   //iterate through all uids in list
@@ -407,11 +398,11 @@ bool scanTag(uint8_t uid[])
       }
       break;
     default:
-    break;
+      break;
   }
   return false;
 }
-
+//Empty EEPROM and set state to adding master tag
 void clearEEPROM()
 {
     for (int i = 0; i <= (maxUIDCount + 2) * 7; i++) {
